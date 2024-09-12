49c578930ade270446486e06225c8d7f5d6651a5
Abu Osama <s.islam20000@gmail.com>
2024-09-12 20:51:11 +0530

    hh

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>submit</Button>
+    </>
+  );
+}
+
+export default App;
